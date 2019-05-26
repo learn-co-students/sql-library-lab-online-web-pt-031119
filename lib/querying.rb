446a4e713310c79b942114c8986ee0e2173d8f0c
github username <github email address>
2019-05-26 11:27:20 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,7 +39,7 @@ def select_series_title_with_most_human_characters
   ON character_books.character_id = characters.id 
   WHERE characters.species = 'human' 
   GROUP BY series.title 
-  ORDER BY COUNT(*) DESC 
+  ORDER BY COUNT(*) DESC
   LIMIT 1;"
 end
 
